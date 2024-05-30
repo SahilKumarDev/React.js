e62821a03bddc632e09e15a467b985c91bb9f35d
NSxVillan <Sahilkumar710@outlook.com>
2024-05-30 11:25:51 +0530

    what is jsx

--- 01_project/src/App.jsx
+++ 01_project/src/App.jsx
@@ -5,6 +5,32 @@ const App = () => {
   return (
     <h1>Hello, villan</h1>
   )
-}
+};
 
 export default App
+
+// // This is behind the scene working
+// import React from 'react'
+// import "./App.css"
+
+// const App = () => {
+//   return (
+//     React.createElement("h1", null, "Hello Villan")
+//   )
+// }
+
+// export default App
+
+
+// // Jsx Console code
+// import React from 'react'
+// import "./App.css"
+
+// const App = () => {
+//   console.log(React.createElement("h1", null, "Hello Villan"));
+//   return (
+//     React.createElement("h1", null, "Hello Villan") 
+//   )
+//   }
+
+// export default App
